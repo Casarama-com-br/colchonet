984368d0e2b61f2dbb89095292e8643c8f00db84
Diogenes <ti@casarama.com.br>
2014-07-28 17:38:47 -0300

    adicionando mais views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,9 @@ class User < ActiveRecord::Base
    validates_presence_of :email , :full_name , :location , :password
    validates_confirmation_of :password
    validates_length_of :bio , minimum: 30 , allow_black: false
-   validate :email_format
+   validates_uniqueness_of :email
    
-   private
-   def email_format
-      errors.add(:email , :invalid) unless
-         email.match(EMAIL_REGEXP)
+   validate do
+      errors.add(:email , :invalid) unless email.match(EMAIL_REGEXP)
    end
 end
