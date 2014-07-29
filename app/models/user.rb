f1292635e278c157f45a87d3b7e9c657131bff3d
Diogenes <ti@casarama.com.br>
2014-07-29 14:45:37 -0300

    muito coisa pra commitar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ActiveRecord::Base
    EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
-   validates_presence_of :email , :full_name , :location , :password
-   validates_confirmation_of :password
+   validates_presence_of :email , :full_name , :location
    validates_length_of :bio , minimum: 30 , allow_black: false
    validates_uniqueness_of :email
-   
+   has_secure_password
    validate do
       errors.add(:email , :invalid) unless email.match(EMAIL_REGEXP)
    end
